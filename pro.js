dc286103e20eb18931af220e09dc28c08a31cf2a
jEuEaEn <jarm1981avrm@gmail.com>
2025-07-24 20:08:23 -0500

    version final

---
+++ pro.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("intt")
+const contenedor = document.getElementById("elementos")
+
+let elementos = []
+
+function agregarElemento(){
+
+    if (input.value != ""){
+
+        elementos.push(input.value)
+
+        let resultado = ""
+        for( let i = 0; i < elementos.length; i++){
+
+            resultado += "<span>" + elementos[i] +"</span>"
+
+        }
+        contenedor.innerHTML = resultado
+
+    }
+    
+}
